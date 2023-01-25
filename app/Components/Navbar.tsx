592e550b8f94ee676b3a4a213d2f8e94711f45e5
Hanzala-Ahmed <hanzala.ahmed181@gmail.com>
2023-01-26 03:33:42 +0500

    first commit

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Kavoon } from "@next/font/google";
+
+const kavoon = Kavoon({ subsets: ["latin"], weight: "400" });
+
+const Navbar = () => {
+  return (
+    <div className=" h-20 w-[100vw] flex justify-evenly items-center">
+      <div
+        className={`${kavoon.className} w-[40%] h-[70%] flex items-center text-2xl`}
+      >
+        Hanzala Ahmed
+      </div>
+      <div className="w-[40%] h-[80%] flex justify-end items-center text-2xl cursor-pointer">
+        <label htmlFor="burger" className="burger">
+          <input id="burger" type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
