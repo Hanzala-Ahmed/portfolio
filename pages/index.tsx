256c50d11e7dde008d5ff3488336b6904834e936
Hanzala-Ahmed <hanzala.ahmed181@gmail.com>
2023-04-17 01:41:37 +0500

    feat: started creating portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,103 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { DynaPuff, Titillium_Web } from "next/font/google";
+import Link from "next/link";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const titilliumWeb = Titillium_Web({
+  subsets: ["latin"],
+  weight: "200",
+});
+const dynaPuff = DynaPuff({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <title>Hanzala Ahmed - Software Engineer | Frontend Developer</title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
         />
-      </div>
+      </Head>
+      <main
+        className={`${titilliumWeb.className} flex min-h-screen flex-col items-center px-24`}
+      >
+        <div className="h-20 w-full flex justify-between">
+          <div className="w-[25%] h-full flex items-center">
+            <div
+              className={`${dynaPuff.className} text-3xl text-blueText`}
+            >{`{Hanzala Ahmed}`}</div>
+          </div>
+          <div className="h-full gap-12 flex items-center justify-between">
+            <ul className="flex gap-7 text-grayText">
+              <li className="text-lg">
+                <Link href={""}>Home</Link>
+              </li>
+              <li className="text-lg">
+                <Link href={""}>About</Link>
+              </li>
+              <li className="text-lg">
+                <Link href={""}>Tech Stack</Link>
+              </li>
+              <li className="text-lg">
+                <Link href={""}>Projects</Link>
+              </li>
+              <li className="text-lg">
+                <Link href={""}>Contact</Link>
+              </li>
+            </ul>
+            <div className="gap-7 flex">
+              <Link href={""}>
+                <Image src={"/githubIcon.svg"} alt="" width={25} height={25} />
+              </Link>
+              <Link href={""}>
+                <Image src={"/twitterIcon.svg"} alt="" width={27} height={27} />
+              </Link>
+              <Link href={""}>
+                <Image
+                  src={"/linkedInIcon.svg"}
+                  alt=""
+                  width={26}
+                  height={26}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="h-full w-full mt-10 flex justify-between items-center py-10 mb-20 border-b-2">
+          <div className="h-full w-[60%] text-justify flex justify-center flex-col text-xl">
+            <p className="mb-5">
+              {`Hi 🤝! My name is Hanzala Ahmed and I'm a frontend developer with
+            2 years of experience. I've always been passionate
+            about creating beautiful and functional websites and web
+            applications, and I love the constant challenge of keeping up with
+            the latest technologies and design trends.`}
+            </p>
+            <p className="mb-5">
+              {`I have a strong background in HTML, CSS, JavaScript, and various frontend 
+            frameworks like React and Next.Js. I'm also skilled in UX/UI design, 
+            responsive design, and cross-browser compatibility. I've worked on a wide range
+            of projects, from small business websites to large-scale web applications, and 
+            I'm always eager to take on new challenges.`}
+            </p>
+          </div>
+          <div className="w-[35%] h-full flex justify-center items-center">
+            <div className="w-72 h-72 border-4 border-blueBorder p-2 rounded-full">
+              <Image
+                src={"/pic.jpg"}
+                alt=""
+                width={400}
+                height={400}
+                className="w-full h-full rounded-full"
+              />
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="h-[400px] w-full mt-10 flex justify-between items-center py-10 border-b-2 mb-20"></div>
+      </main>
+    </>
+  );
 }
